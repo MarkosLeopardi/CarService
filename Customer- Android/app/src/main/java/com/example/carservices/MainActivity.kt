997b17472f005047135de023e3a7740d666f2688
MarkosLeopardi <markleoleo@gmail.com>
2024-06-07 14:46:32 +0300

    Android Changes

--- Customer- Android/app/src/main/java/com/example/carservices/MainActivity.kt
+++ Customer- Android/app/src/main/java/com/example/carservices/MainActivity.kt
@@ -4,27 +4,49 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    var toolbar: Toolbar? = null
+    private lateinit var toolbar: Toolbar
     private lateinit var log: Button
     private lateinit var user: EditText
     private lateinit var pass: EditText
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.title = "LOGIN"
 
-        log = findViewById(R.id.login)
-        log.setOnClickListener{
-            // Edw anoiga to epomeno screen
-        }
+        firebaseAuth = FirebaseAuth.getInstance()
 
+        user = findViewById(R.id.Email)
+        pass = findViewById(R.id.Password)
+        log = findViewById(R.id.login)
+
+        log.setOnClickListener {
+            val email = user.text.toString()
+            val password = pass.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, CustomerActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
